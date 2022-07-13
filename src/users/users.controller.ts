75d24cf2974e4cfe5f7a26a821240970b79e6d6f
Dina Shchobova <dina.shchobova@gmail.com>
2022-07-14 02:30:53 +0300

    feat: add UseInterceptors

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,37 +6,60 @@ import {
   Param,
   Delete,
   Put,
+  HttpCode,
+  ParseUUIDPipe,
+  ClassSerializerInterceptor,
+  UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
-@Controller('users')
+@Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @HttpCode(201)
+  create(@Body() createUserDto: CreateUserDto): User {
+    return new User({
+      ...this.usersService.create(createUserDto),
+    });
   }
 
   @Get()
+  @HttpCode(200)
   findAll() {
     return this.usersService.findAll();
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
+  @HttpCode(200)
+  findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return new User({
+      ...this.usersService.findOne(id),
+    });
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(id, updateUserDto);
+  @HttpCode(200)
+  update(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    console.log('updateUserDto = ', updateUserDto);
+    return new User({
+      ...this.usersService.update(id, updateUserDto),
+    });
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  @HttpCode(204)
+  remove(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
     return this.usersService.remove(id);
   }
 }
