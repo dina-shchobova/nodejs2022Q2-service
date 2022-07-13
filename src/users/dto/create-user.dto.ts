2667b4be58475435345c254dc668d429fc9b536b
Dina Shchobova <dina.shchobova@gmail.com>
2022-07-14 02:31:28 +0300

    fix: delete comments

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,3 @@
-//import { Exclude } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
@@ -6,12 +5,7 @@ export class CreateUserDto {
   @IsNotEmpty()
   login: string;
 
-  // @Exclude()
   @IsString()
   @IsNotEmpty()
   password: string;
-
-  // version: number; // integer number, increments on update
-  // createdAt: number; // timestamp of creation
-  // updatedAt: number; // timestamp of last update
 }
