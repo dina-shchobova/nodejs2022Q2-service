34c92bb47fa5149755ff2639dfdc7c4c53389b1d
Dina Shchobova <dina.shchobova@gmail.com>
2022-07-13 23:15:22 +0300

    feat: set up server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { dirname, join } from 'path';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  SwaggerModule.setup('doc', app, document);
   await app.listen(4000);
 }
 bootstrap();
