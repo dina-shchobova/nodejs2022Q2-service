1dd9fbe99739bca6c72cbe9ca1078e7610c4d287
Dina Shchobova <dina.shchobova@gmail.com>
2022-07-13 23:50:07 +0300

    feat: save PORT value in .env file.

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { dirname, join } from 'path';
 import { readFile } from 'fs/promises';
 import { parse } from 'yaml';
+import 'dotenv/config';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 4001;
   const app = await NestFactory.create(AppModule);
   const rootDirname = dirname(__dirname);
   const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+
   const document = parse(DOC_API);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
   SwaggerModule.setup('doc', app, document);
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
