b97f61f09b9e9233dd9799209a46432661220595
Dina Shchobova <dina.shchobova@gmail.com>
2022-07-14 18:16:45 +0300

    feat: add tracks in data

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,5 +1,6 @@
-import { IAlbum, Artist, User } from '../utils/interfaces';
+import { IAlbum, Artist, User, ITrack } from '../utils/interfaces';
 
 export const users: User[] = [];
 export const artists: Artist[] = [];
 export const albums: IAlbum[] = [];
+export const tracks: ITrack[] = [];
