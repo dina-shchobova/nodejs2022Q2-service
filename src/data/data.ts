aea744373622d7b1ef028e0979cbf8b1f5c1097a
Dina Shchobova <dina.shchobova@gmail.com>
2022-07-16 18:32:05 +0300

    feat: add favs in data

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,6 +1,11 @@
-import { IAlbum, Artist, User, ITrack } from '../utils/interfaces';
+import { IAlbum, Artist, User, ITrack, IFavorites } from '../utils/interfaces';
 
 export const users: User[] = [];
 export const artists: Artist[] = [];
 export const albums: IAlbum[] = [];
 export const tracks: ITrack[] = [];
+export const favs: IFavorites = {
+  artists: [],
+  albums: [],
+  tracks: [],
+};
