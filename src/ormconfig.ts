8c1ee6213994fdf3b1968d3ae5b0d653a4b91b24
Dina Shchobova <dina.shchobova@gmail.com>
2022-07-27 10:56:36 +0300

    feat: add ormconfig

---
+++ src/ormconfig.ts
@@ -0,0 +1,17 @@
+import { DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: +process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: false,
+  entities: ['dist/**/entities/*.entity.js'],
+  migrations: ['dist/**/migrations/*.js'],
+  migrationsRun: true,
+} as DataSourceOptions;
