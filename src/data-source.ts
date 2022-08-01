5dd9e1c78005aed32cff9f181c05f12335f49bf2
Dina Shchobova <dina.shchobova@gmail.com>
2022-08-01 12:38:44 +0300

    fix: fix data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,14 +5,17 @@ import { User } from './users/entities/user.entity';
 import { Album } from './albums/entities/album.entity';
 import { Track } from './tracks/entities/track.entity';
 import { Favorite } from './favorites/entities/favorites.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: +process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   synchronize: false,
   logging: true,
   entities: [User, Artist, Album, Track, Favorite],
